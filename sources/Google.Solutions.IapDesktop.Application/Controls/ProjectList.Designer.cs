5d1fdbd43b963bcbf620e06d00072c4958743b9b
Johannes Passing <jpassing@google.com>
2020-07-30 15:14:41 +0200

    WIP

---
+++ sources/Google.Solutions.IapDesktop.Application/Controls/ProjectList.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Google.Solutions.IapDesktop.Application.Controls
+{
+    partial class ProjectList
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.list = new Google.Solutions.IapDesktop.Application.Controls.ProjectsListView();
+            this.nameColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.idColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(0, 0);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(250, 3);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 0;
+            // 
+            // list
+            // 
+            this.list.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.list.AutoResizeColumnsOnUpdate = false;
+            this.list.CheckBoxes = true;
+            this.list.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.nameColumnHeader,
+            this.idColumnHeader});
+            this.list.HideSelection = false;
+            this.list.Location = new System.Drawing.Point(0, 3);
+            this.list.Name = "list";
+            this.list.OwnerDraw = true;
+            this.list.SelectedModelItem = null;
+            this.list.Size = new System.Drawing.Size(250, 200);
+            this.list.TabIndex = 1;
+            this.list.UseCompatibleStateImageBehavior = false;
+            this.list.View = System.Windows.Forms.View.Details;
+            this.list.Layout += new System.Windows.Forms.LayoutEventHandler(this.list_Layout);
+            // 
+            // nameColumnHeader
+            // 
+            this.nameColumnHeader.Text = "Name";
+            this.nameColumnHeader.Width = 100;
+            // 
+            // idColumnHeader
+            // 
+            this.idColumnHeader.Text = "ID";
+            // 
+            // ProjectList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.progressBar);
+            this.Name = "ProjectList";
+            this.Size = new System.Drawing.Size(250, 200);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private ProjectsListView list;
+        private System.Windows.Forms.ColumnHeader nameColumnHeader;
+        private System.Windows.Forms.ColumnHeader idColumnHeader;
+    }
+}
---
+++ sources/Google.Solutions.IapDesktop.Application/Controls/ProjectList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using Google.Apis.Compute.v1.Data;
+
+namespace Google.Solutions.IapDesktop.Application.Controls
+{
+    public partial class ProjectList : UserControl
+    {
+        public event EventHandler LoadingChanged;
+
+        public ProjectList()
+        {
+            InitializeComponent();
+
+        }
+
+        public bool Loading 
+        {
+            get => this.progressBar.Enabled;
+            set
+            {
+                this.progressBar.Enabled = value;
+                this.LoadingChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public void BindItems(ObservableCollection<Project> model)
+        {
+            this.list.BindCollection(model);
+        }
+
+        public void BindSelectedItems(ObservableCollection<Project> model)
+        {
+            throw new NotImplementedException();
+        }
+
+        //---------------------------------------------------------------------
+        // Window events.
+        //---------------------------------------------------------------------
+
+        private void list_Layout(object sender, LayoutEventArgs e)
+        {
+            this.nameColumnHeader.Width = (int)(this.Width/2);
+            this.idColumnHeader.Width = this.Width - 
+                this.nameColumnHeader.Width - 
+                SystemInformation.VerticalScrollBarWidth;
+        }
+    }
+
+    internal class ProjectsListView : BindableListView<Project>
+    {
+    }
+
+}
